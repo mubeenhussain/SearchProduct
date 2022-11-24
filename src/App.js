a2d7d178c6e94952afb00c516d4520a3e4f79d18
mubeenhussain <mubeenhussain8@gmail.com>
2022-11-24 23:41:24 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect,useMemo,useState} from 'react'
+import SearchBox from './Components/SearchBox';
+import Card from './Components/Card';
+import axios from 'axios';
+import Heading from './Components/Heading';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+
+
+  useEffect(()=>{
+    axios.get('https://services-dev.youonline.online/api/get_automotive_make_and_models/')
+    .then((response)=>{
+      setData(response?.data?.response);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  },[])
+
+  const filteredArray = useMemo(()=>{  
+    if(search!==''){
+      let products = [];
+    for ( const brand of data ){
+    const modal =  brand.atuomotivemodel_automotivemake.filter( (item,index) => {          
+            if(item.title.toLowerCase().includes(search.toLowerCase()) ){
+              return item;
+            }
+         })
+    if(modal.length>0){
+      products.push({title:brand.title,modal});
+    }
+    }
+    return products;
+    }  
+  },[search])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display:"flex",justifyContent:"center"}}>
+      <div style={{display:"flex",flexDirection:"column",width:"75%"}}>
+
+        <SearchBox className="mt-2" search={search} setSearch={setSearch} filteredArray={filteredArray} />
+        {search === ''? data?.map((brand)=>(
+          <>
+          <Heading>
+            {brand?.title}
+          </Heading>
+          
+          <div class="grid grid-cols-4 gap-4">
+          {
+            brand?.atuomotivemodel_automotivemake?.map((model)=>(
+              <div>
+                <Card title={model.title} year={model?.year} data={data} />
+              </div>
+              ))
+            }         
+          </div>
+          
+          </>
+        ))
+        :
+        filteredArray?.map((brand)=>(
+          <>
+          <Heading>
+            {brand?.title}
+          </Heading>
+          
+          <div class="grid grid-cols-4 gap-4">
+          {
+            brand?.modal?.map((model)=>(
+              <div>
+                <Card title={model.title} year={model?.year} data={data} />
+              </div>
+              ))
+            }         
+          </div>
+          
+          </>
+        ))
+      }
+      </div>
     </div>
   );
 }
---
+++ src/Components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Heading({children}) {
+  return (
+    <div><h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-neutral-600 text-left mb-3 mt-5">{children}</h3></div>
+  )
+}
+
+export default Heading
